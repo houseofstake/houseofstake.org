017df20685fe43184aa353764dac6dcf31a3a7d2
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-08-09 11:24:56 +0300

    katex update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,10 +75,10 @@ const config: Config = {
 
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css',
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.css',
       type: 'text/css',
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+        'sha384-5TcZemv2l/9On385z///+d7MSYlvIEw9FuZTIdZ14vJLqWphw7e7ZPuOiCHJcFCP',
       crossorigin: 'anonymous',
     },
   ],
